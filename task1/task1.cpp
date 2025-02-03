b795fc49bee7eeca09c0fa8f2473ae931478a58f
p1xta <daria.petrova496@gmail.com>
2025-02-03 20:37:35 +0700

    task1 into main

---
+++ task1/task1.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include <cmath>
+#include <iomanip>
+
+#define size 10000000
+
+#ifdef USE_DOUBLE
+        using ArrayType = double;
+#else
+        using ArrayType = float;
+#endif
+
+
+int main() {
+    ArrayType* sin = new ArrayType[size];
+    ArrayType sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        #ifdef USE_DOUBLE
+            sin[i] = std::sin((ArrayType)i * 2.0 * M_PI / size);
+        #else 
+            sin[i] = sinf((ArrayType)i * 2.0 * M_PI / size);
+        #endif
+        sum += sin[i];
+    }
+    std::cout << std::setprecision(9);
+    std::cout << sum << std::endl;
+    
+    delete[] sin;
+    return 0;
+}
